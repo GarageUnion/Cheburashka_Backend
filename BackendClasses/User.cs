6379ee0d249b74010e5f5ae2fcd91c6a8a742dea
22222 11111 <vanya.popov.2003@gmail.com>
2023-05-08 21:17:55 +0300

    Добавьте файлы проекта.

---
+++ BackendClasses/User.cs
@@ -0,0 +1,17 @@
+﻿using System.Formats.Asn1;
+
+namespace BackendClasses
+{
+    public class User
+    {
+        int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public DateTime Birthday { get; set; }
+        public string? Description { get; set; }
+        public string? Status { get; set; }
+        public List<string> Photos { get; set; } //пути к фоткам, фотки будут лежать в облаке
+        public List<string>? Tags { get; set; }
+    }
+}
